c6dc4e54c951a37fc931ffd3324396ad88c160bc
ZaretskiyM <zaretwork@gmail.com>
2023-07-29 14:06:08 +0300

    Init com

---
+++ src/main/java/ru/hogwarts/school/SkyProHogwartsApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHogwartsApplication.class, args);
+    }
+
+}
